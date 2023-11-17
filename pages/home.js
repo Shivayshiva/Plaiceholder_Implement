fd0dbffbc0cbf1aa6bd0f84319b8636faed5bb0d
Shivayshiva <prashantmaddheshiya@gmail.com>
2023-11-17 16:23:37 +0530

    Changes made in utils/blur-data-generator.js, home.js

--- pages/home.js
+++ pages/home.js
@@ -1,19 +1,14 @@
 import React from 'react'
 import getBlurData from './utils/blur-data-generator';
 import Image from 'next/image';
-import Img from '../public/photo1.jpg'
+//import Img from '../public/photo1.jpg'
 
 
 
 export default async function home() {
-   const imageUrl= "https://images.unsplash.com/photo-1698338956399-bf6a91f44de5?q=80&w=1924&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const imageUrl= "https://images.unsplash.com/photo-1698338956399-bf6a91f44de5?q=80&w=1924&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const base64 = await getBlurData(imageUrl);
   
-  const base64 = await getBlurData();
-    //async function handleClick(){
-    //    const data= await fetch("/api/hello");
-    //    const dataDT= await data.json()
-    //    console.log(dataDT);
-    //}
   return (
     <div>
       <button >Click Me</button>
--- pages/utils/blur-data-generator.js
+++ pages/utils/blur-data-generator.js
@@ -1,21 +1,12 @@
 import { getPlaiceholder } from "plaiceholder";
 
+export default async function getBlurData(imageUrl){
+  try {
+    const buffer = await fetch(imageUrl).then(async (res) => { return Buffer.from(await res.arrayBuffer()) });
+    const { base64 } = await getPlaiceholder(buffer);
 
-export default async function getBlurData(){
-try {
-    const imageUrl= "https://images.unsplash.com/photo-1698338956399-bf6a91f44de5?q=80&w=1924&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-
-  const buffer = await fetch(imageUrl).then(async (res) =>{
-  console.log('RES_body',res.body);
-    Buffer.from(await res.arrayBuffer())
-});
-
-  console.log("BUFFEREER", buffer);
-  const { base64 } = await getPlaiceholder(file);
-  console.log("BASE64", base64);
-  return base64
- 
-} catch (err) {
-  err;
-}
+    return base64
+  } catch (err) {
+    err;
+  }
 }
